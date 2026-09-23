7b6c19817aae0dea6e2eea679b3ba6fb20d16a48
Patrick Sims <234958733+thebrazenbeard@users.noreply.github.com>
2026-09-23 05:25:42 -0400

    Remove stray governed-subject assignment from run receipt

--- src/driftguard/benchmark.py
+++ src/driftguard/benchmark.py
@@ -864,11 +864,6 @@ class BenchmarkRunReceipt:
                 "BenchmarkRunReceipt must come from run_precommitted_holdout"
             )
         object.__setattr__(self, "study_id", study_id)
-        object.__setattr__(
-            self,
-            "study_subject_digest",
-            study_subject_digest,
-        )
         object.__setattr__(self, "attempt_id", attempt_id)
         object.__setattr__(self, "precommit_digest", precommit_digest)
         object.__setattr__(
